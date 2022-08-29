d34eb2b54d530d5f9412927315201207658c4d27
Tong Quang Minh <quangminh1520111@gmail.com>
2022-08-29 21:59:30 +0700

    fx

--- Nodejs-Passport-Login-master/passport-config.js
+++ Nodejs-Passport-Login-master/passport-config.js
@@ -1,29 +1,67 @@
-const LocalStrategy = require('passport-local').Strategy
-const bcrypt = require('bcrypt')
+const LocalStrategy = require("passport-local").Strategy;
+const fs = require("fs");
+const bcrypt = require("bcrypt");
 
-function initialize(passport, getUserByEmail, getUserById) {
-  const authenticateUser = async (email, password, done) => {
-    const user = getUserByEmail(email)
-    if (user == null) {
-      return done(null, false, { message: 'No user with that email' })
-    }
+function initialize(passport ) {
+  // getUserByEmail
+  //getUserById
+  // const authenticateUser = async (email, password, done) => {
+  //   const user = getUserByEmail(email);
+  //   if (user == null) {
+  //     return done(null, false, { message: "No user with that email" });
+  //   }
 
-    try {
-      if (await bcrypt.compare(password, user.password)) {
-        return done(null, user)
-      } else {
-        return done(null, false, { message: 'Password incorrect' })
+  //   try {
+  //     if (await bcrypt.compare(password, user.password)) {
+  //       return done(null, user);
+  //     } else {
+  //       return done(null, false, { message: "Password incorrect" });
+  //     }
+  //   } catch (e) {
+  //     return done(e);
+  //   }
+  // };
+  const authenticateUser = (email, password, done) => {
+    fs.readFile("./userDB.json", async (err, data) => {
+      const db = JSON.parse(data);
+      const userRecord = db.find((user) => user.email === email);
+      if (userRecord == null) {
+        return done(null, false, {
+          message: "No user with that email"
+        });
       }
-    } catch (e) {
-      return done(e)
-    }
-  }
+      try {
+        if (await bcrypt.compare(password, userRecord.password)) {
+          console.log({password});
+          console.log(userRecord.password)
+          return done(null, userRecord);
+        } else {
+          return done(null, false, {
+            message: "Password incorrect"
+          });
+        }
+      } catch (e) {
+        return done(e);
+      }
+    });
+  };
 
-  passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser))
-  passport.serializeUser((user, done) => done(null, user.id))
+  passport.use(new LocalStrategy({
+    usernameField: "email"
+  }, authenticateUser));
+  passport.serializeUser((user, done) => done(null, user.id));
   passport.deserializeUser((id, done) => {
-    return done(null, getUserById(id))
-  })
+    // return done(null, getUserById(id));
+    fs.readFile("./userDB.json", (err, data) => {
+      const db = JSON.parse(data);
+      const userRecord = db.find((user) => user.id === id);
+      if (userRecord) {
+        return done(null, userRecord);
+      } else {
+        return done(null, false);
+      }
+    });
+  });
 }
 
-module.exports = initialize
+module.exports = initialize;
--- Nodejs-Passport-Login-master/server.js
+++ Nodejs-Passport-Login-master/server.js
@@ -1,90 +1,123 @@
-// if (process.env.NODE_ENV !== 'production') {
-//   require('dotenv').config()
-// }
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
-const express = require('express')
-const app = express()
-const bcrypt = require('bcrypt')
-const passport = require('passport')
-const flash = require('express-flash')
-const session = require('express-session')
-const methodOverride = require('method-override')
+const express = require("express");
+const app = express();
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
+const methodOverride = require("method-override");
+const fs = require("fs");
 
-const initializePassport = require('./passport-config')
+const initializePassport = require("./passport-config");
+const { json } = require("body-parser");
+
 initializePassport(
   passport,
-  email => users.find(user => user.email === email),
-  id => users.find(user => user.id === id)
-)
+  // (email) => users.find((user) => user.email === email),
+  // (id) => users.find((user) => user.id === id)
+);
 
-const users = []
+// const users = [];
 
-app.set('view-engine', 'ejs')
-app.use(express.urlencoded({ extended: false }))
-app.use(flash())
-app.use(session({
-  // secret: process.env.SESSION_SECRET,
-  // resave: false,
-  // saveUninitialized: false
-  secret: "secret",
-  saveUninitialized: true,
-  resave: true,
-}))
-app.use(passport.initialize())
-app.use(passport.session())
-app.use(methodOverride('_method'))
+app.set("view-engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(flash());
+app.use(
+  session({
+    // secret: process.env.SESSION_SECRET,
+    // resave: false,
+    // saveUninitialized: false
+    secret: "secret",
+    saveUninitialized: true,
+    resave: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride("_method"));
 
-app.get('/', checkAuthenticated, (req, res) => {
-  res.render('index.ejs', { name: req.user.name })
-})
+app.get("/", checkAuthenticated, (req, res) => {
+  res.render("index.ejs", { name: req.user.name });
+});
 
-app.get('/login', checkNotAuthenticated, (req, res) => {
-  res.render('login.ejs')
-})
+app.get("/login", checkNotAuthenticated, (req, res) => {
+  res.render("login.ejs");
+});
 
-app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/login',
-  failureFlash: true
-}))
+app.post(
+  "/login",
+  checkNotAuthenticated,
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
 
-app.get('/register', checkNotAuthenticated, (req, res) => {
-  res.render('register.ejs')
-})
+app.get("/register", checkNotAuthenticated, (req, res) => {
+  res.render("register.ejs");
+});
 
-app.post('/register', checkNotAuthenticated, async (req, res) => {
+app.post("/register", checkNotAuthenticated, async (req, res) => {
   try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10)
-    users.push({
-      id: Date.now().toString(),
-      name: req.body.name,
-      email: req.body.email,
-      password: hashedPassword
-    })
-    res.redirect('/login')
-  } catch {
-    res.redirect('/register')
-  }
-})
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    // users.push({
+    //   id: Date.now().toString(),
+    //   name: req.body.name,
+    //   email: req.body.email,
+    //   password: hashedPassword,
+    // });
 
-app.delete('/logout', (req, res) => {
-  req.logOut()
-  res.redirect('/login')
-})
+    fs.readFile("./userDB.json", (err, data) => {
+      var db = JSON.parse(data);
+      db.push({
+        id: Date.now().toString(),
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword,
+      });
+      console.log(db);
+      newdb = JSON.stringify(db); //convecrt it back to json
+      fs.writeFileSync("./userDB.json", newdb, "utf8");
+    });
+
+    res.redirect("/login");
+  } catch {
+    res.redirect("/register");
+  }
+});
+
+app.post("/logout", function (req, res) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
+
+// app.delete('/logout', (req, res) => {
+//   console.log('test')
+//   req.logOut()
+//   res.redirect('/login')
+// })
 
 function checkAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
-    return next()
+    return next();
   }
 
-  res.redirect('/login')
+  res.redirect("/login");
 }
 
 function checkNotAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
-    return res.redirect('/')
+    return res.redirect("/");
   }
-  next()
+  next();
 }
 
-app.listen(3000)
+app.listen(3000);

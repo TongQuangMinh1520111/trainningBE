203df67999d2a657977d7b6aa858c91bdcefac93
Tong Quang Minh <quangminh1520111@gmail.com>
2022-09-04 19:06:32 +0700

    update

---
+++ reactjs/client/src/App.js
@@ -0,0 +1,28 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import axios from "axios";
+
+function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    axios
+      .get("/api/hello")
+      .then((data) => setData(data.data.message));
+      // .then((data) => {
+      //   console.log({ data });
+      // });
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{!data ? "Loading..." : data}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
